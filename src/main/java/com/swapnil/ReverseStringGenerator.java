70e8f2d5b0c0da4cc615272fc53ac7c0d7090d80
Swapnil Palkar <palkars@users.noreply.github.com>
2023-10-10 18:28:38 +0100

    Reverse string implementation

--- src/main/java/com/swapnil/ReverseStringGenerator.java
+++ src/main/java/com/swapnil/ReverseStringGenerator.java
@@ -2,6 +2,14 @@ package com.swapnil;
 
 public class ReverseStringGenerator {
     public String reverse(String input) {
-        return input;
+        if (input == null || "".equals(input.trim())) {
+            return input;
+        }
+        int length = input.length();
+        char[] reverseBytes = new char[length];
+        for (int i = length-1; i >= 0; i--) {
+            reverseBytes[i] = input.charAt(length -1 - i);
+        }
+        return new String(reverseBytes);
     }
 }
--- src/test/java/com/swapnil/ReverseStringGeneratorTest.groovy
+++ src/test/java/com/swapnil/ReverseStringGeneratorTest.groovy
@@ -4,12 +4,28 @@ import spock.lang.Specification
 
 class ReverseStringGeneratorTest extends Specification {
 
+    ReverseStringGenerator reverseStringGenerator
+
     void setup() {
+        reverseStringGenerator = new ReverseStringGenerator()
     }
 
     void cleanup() {
     }
 
     def "Reverse"() {
+        when:
+        def actualOutput = reverseStringGenerator.reverse(input)
+
+        then:
+        actualOutput == expectedOutput
+
+        where:
+        input  | expectedOutput
+        ""     | ""
+        null   | null
+        "    " | "    "
+        "abc"  | "cba"
+
     }
 }

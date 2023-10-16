fb7c8f0d7859a28791221342fb5d09aacb41d35e
Swapnil Palkar <palkars@users.noreply.github.com>
2023-10-16 10:15:06 +0100

    interview preparation

--- src/main/java/com/swapnil/NumberReversal.java
+++
@@ -1,8 +0,0 @@
-package com.swapnil;
-
-public class NumberReversal {
-    ReverseStringGenerator reverseStringGenerator = new ReverseStringGenerator();
-    public int reverse(int input) {
-        return Integer.parseInt(reverseStringGenerator.reverse(input+""));
-    }
-}
